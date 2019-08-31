b11d067de305946f08b36bfa12971fb227487ff3
MarianVytak <marianvytak@gmail.com>
2019-08-31 04:01:38 +0300

    Changed work Catalog in Header

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -2,7 +2,34 @@ import $ from  'jquery';
 
 'use strict';
 
-$('.header__bot_catalog_link').on('click', function (e) {
+const catalogLink = $('.header__bot_catalog_link');
+const catalogList = $('.header__bot_catalog_list');
+const catalogContentLink = $('.header__bot_catalog_list_item_link');
+const catalogContentList = $('.header__bot_catalog_list_item_content');
+
+// Відкрити/закрити каталог товарів у Header
+catalogLink.on('click', function (e) {
     e.preventDefault();
-    $('.header__bot_catalog_list').slideToggle();
+    catalogList.slideToggle();
+    catalogContentLink.removeClass('active');
+    catalogContentList.slideUp();
+    catalogContentList.removeClass('active');
+});
+
+// Відкрити/закрити боковий список у Каталозі Товарів
+catalogContentLink.on('click', function (e) {
+    e.preventDefault();
+    let catalogContentListThis = $(this).attr('href');
+
+    if (!$(this).hasClass('active')) {
+        catalogContentLink.removeClass('active');
+    }
+    $(this).toggleClass('active');
+
+    if (!$(catalogContentListThis).hasClass('active')) {
+        catalogContentList.slideUp();
+        catalogContentList.removeClass('active');
+    }
+    $(catalogContentListThis).slideToggle();
+    $(catalogContentListThis).toggleClass('active');
 });
