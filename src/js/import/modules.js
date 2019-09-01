550de61e38be2ca5e0c3d818cfa717c933dbef88
MarianVytak <marianvytak@gmail.com>
2019-09-02 02:14:06 +0300

    Added Titles

--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,6 +1,8 @@
 import "%modules%/header/header";
 import "%modules%/home/home";
 import "%modules%/sidebar/sidebar";
+import "%modules%/cooperation/cooperation";
+import "%modules%/viewed-products/viewed-products";
 import "%modules%/footer/footer";
 
 import "./libs/range";
