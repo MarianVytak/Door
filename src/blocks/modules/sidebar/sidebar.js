d33459484e5068b94e7d55335990f8575beba641
MarianVytak <marianvytak@gmail.com>
2019-09-03 00:29:11 +0300

    Added custom Scrollbar

--- src/blocks/modules/sidebar/sidebar.js
+++ src/blocks/modules/sidebar/sidebar.js
@@ -1,4 +1,5 @@
 import $ from 'jquery';
+import '../../../../node_modules/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min';
 
 'use strict';
 
@@ -6,3 +7,11 @@ $('.sidebar__colors .custom-control').on('click', function (e) {
     e.preventDefault();
     $(this).toggleClass('active');
 });
+
+(function($){
+    $(window).on("load",function(){
+        $(".sidebar__check_list").mCustomScrollbar({
+            theme:"dark-3"
+        });
+    });
+})(jQuery);
