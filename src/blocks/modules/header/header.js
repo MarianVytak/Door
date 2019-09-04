e04933e685fe5568c56e1f4ad58a36b37388fd75
MarianVytak <marianvytak@gmail.com>
2019-09-04 11:58:22 +0300

    Перемістив каталог товарів у сайдбар

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -2,19 +2,19 @@ import $ from  'jquery';
 
 'use strict';
 
-const catalogLink = $('.header__bot_catalog_link');
-const catalogList = $('.header__bot_catalog_list');
+// const catalogLink = $('.header__bot_catalog_link');
+// const catalogList = $('.header__bot_catalog_list');
 const catalogContentLink = $('.header__bot_catalog_list_item_link');
 const catalogContentList = $('.header__bot_catalog_list_item_content');
 
 // Відкрити/закрити каталог товарів у Header
-catalogLink.on('click', function (e) {
-    e.preventDefault();
-    catalogList.slideToggle();
-    catalogContentLink.removeClass('active');
-    catalogContentList.slideUp();
-    catalogContentList.removeClass('active');
-});
+// catalogLink.on('click', function (e) {
+//     e.preventDefault();
+//     catalogList.slideToggle();
+//     catalogContentLink.removeClass('active');
+//     catalogContentList.slideUp();
+//     catalogContentList.removeClass('active');
+// });
 
 // Відкрити/закрити боковий список у Каталозі Товарів
 catalogContentLink.on('click', function (e) {
