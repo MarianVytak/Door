5f12b95dcc396b575fab6198e1ff90a843fa18ab
MarianVytak <marianvytak@gmail.com>
2019-09-09 18:09:41 +0300

    Переробив навігацію по сайту у сайдбарі.

--- src/blocks/modules/navigation/navigation.js
+++ src/blocks/modules/navigation/navigation.js
@@ -8,7 +8,7 @@ const catalogContentList = $('.navigation__list_item_content');
 // Відкрити/закрити боковий список у Каталозі Товарів
 catalogContentLink.on('click', function (e) {
     e.preventDefault();
-    let catalogContentListThis = $(this).attr('href');
+    let catalogContentListThis = $(this).attr('data-target');
 
     if (!$(this).hasClass('active')) {
         catalogContentLink.removeClass('active');
