9855a94e3356821fe832b78337b819ea775d18a3
MarianVytak <marianvytak@gmail.com>
2019-09-12 22:06:11 +0300

    Зробив сторінку Product.

--- src/blocks/modules/product/product.js
+++ src/blocks/modules/product/product.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 window.$ = window.jQuery = $;
 import 'slick-carousel';
 import '../../../../node_modules/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min';
+import "../../../../node_modules/bootstrap/js/src/tab";
 
 'use strict';
 
