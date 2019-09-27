7f3197c4f989153691104fc411bd45d23c63f7fc
MarianVytak <marianvytak@gmail.com>
2019-09-27 10:52:27 +0300

    Basket: добавив функціонал для форм при виборі способу оплати.

--- src/blocks/modules/basket/basket.js
+++ src/blocks/modules/basket/basket.js
@@ -6,3 +6,18 @@ $('.basket__sidebar_coupon_link').on('click', function (e) {
     e.preventDefault();
     $('.basket__sidebar_coupon').fadeToggle();
 });
+
+const basketPaymentLink = $('.basket__payment_link');
+const basketPaymentContent = $('.basket__payment_content');
+basketPaymentLink.on('click', function () {
+    basketPaymentContent.removeClass('active');
+    let thisBasketPaymentContent = $(this).attr('data-target');
+    $(thisBasketPaymentContent).addClass('active');
+});
+const basketPaymentLinkChild = $('.basket__payment_link_child');
+const basketPaymentContentChild = $('.basket__payment_content_child');
+basketPaymentLinkChild.on('click', function () {
+    basketPaymentContentChild.removeClass('active');
+    let thisBasketPaymentContentChild = $(this).attr('data-target');
+    $(thisBasketPaymentContentChild).addClass('active');
+});
