a18f95eeb2522b50b7555bd803c8ad53365afdf8
MarianVytak <marianvytak@gmail.com>
2019-10-29 17:08:36 +0200

    Badket: вніс список правок.

--- src/blocks/modules/basket/basket.js
+++ src/blocks/modules/basket/basket.js
@@ -7,6 +7,7 @@ $('.basket__sidebar_coupon_link').on('click', function (e) {
     $('.basket__sidebar_coupon').fadeToggle();
 });
 
+
 const basketPaymentLink = $('.basket__payment_link');
 const basketPaymentContent = $('.basket__payment_content');
 basketPaymentLink.on('click', function () {
@@ -20,4 +21,22 @@ basketPaymentLinkChild.on('click', function () {
     basketPaymentContentChild.removeClass('active');
     let thisBasketPaymentContentChild = $(this).attr('data-target');
     $(thisBasketPaymentContentChild).addClass('active');
+});
+
+
+const basketCalculationLink = $('.basket__calculation_link');
+const basketCalculationContent = $('.basket__calculation_content');
+basketCalculationLink.on('click', function () {
+    let basketCalculationContentThis = $(this).attr('data-target');
+    basketCalculationContent.removeClass('active');
+    $(basketCalculationContentThis).addClass('active');
+});
+
+
+const calculationLink = $('.calculation__link');
+const calculationContent = $('.calculation__content');
+calculationLink.on('click', function () {
+    let calculationContentThis = $(this).attr('data-target');
+    calculationContent.removeClass('active');
+    $(calculationContentThis).addClass('active');
 });
